fec533654f5715f4f68d12fe02df22ed287aa2fa
Teplyakov Vladislav Vitalievich <somi.saaw@mail.ru>
2024-03-15 21:04:50 +0300

    Добавьте файлы проекта.

---
+++ GIT_Module-3_KDZ-3/Program.cs
@@ -0,0 +1,30 @@
+﻿using Telegram.Bot;
+using Telegram.Bot.Types;
+internal class Program
+{
+    static void Main(string[] args)
+    {
+        var client = new TelegramBotClient("6757896301:AAGpTPCVQBwt0dkJN5wYG4MD3dCmmUJWGyg");
+        client.StartReceiving(Update, Error);
+        Console.ReadLine();
+    }
+
+    async static Task Update(ITelegramBotClient botClient, Update update, CancellationToken token)
+    {
+        var message = update.Message;
+        if (message.Text == null)
+        {
+            if (message.Text.ToLower().Contains("здорова"))
+            {
+                await botClient.SendTextMessageAsync(message.Chat.Id, "Здорова пользователь");
+                return;
+            }
+        }
+    }
+
+    async static Task Error(ITelegramBotClient client, Exception exception, CancellationToken token)
+    {
+        throw new NotImplementedException();
+    }
+}
+
