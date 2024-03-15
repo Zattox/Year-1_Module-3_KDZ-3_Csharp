3e2a8df75d213694968cd765e9295c8d53dba259
Teplyakov Vladislav Vitalievich <somi.saaw@mail.ru>
2024-03-15 23:56:16 +0300

    Добавил класс GeraldicSignList

---
+++ ClassLibrary/GeraldicSignList.cs
@@ -0,0 +1,54 @@
+﻿using System.Text.Json;
+using System.Text.Json.Serialization;
+using System.Xml.Linq;
+internal class GeraldicSignList
+{
+    private const int countOfHeaders = 9;
+    private List<string> headersEng;
+    private List<string> headersRus;
+    private List<GeraldicSign> data;
+
+    public GeraldicSignList()
+    {
+        headersEng = new List<string>(countOfHeaders);
+        headersRus = new List<string>(countOfHeaders);
+        data = new List<GeraldicSign>();
+    }
+    public GeraldicSignList(List<string> headersEng, List<string> headersRus, List<GeraldicSign> data)
+    {
+        this.headersEng = headersEng;
+        this.headersRus = headersRus;
+        this.data = data;
+    }
+    [JsonPropertyName("HeadersEng")]
+    public List<string> HeadersEng => headersEng;
+
+    [JsonPropertyName("HeadersRus")]
+    public List<string> HeadersRus => headersRus;
+
+    [JsonPropertyName("Data")]
+    public List<GeraldicSign> Data => data;
+
+    public string ToJson()
+    {
+        return JsonSerializer.Serialize(this);
+    }
+    public override string ToString()
+    {
+        string result = string.Empty;
+        foreach (string header in headersEng)
+        {
+            result += $"\"{header}\";";
+        }
+        foreach (string header in headersRus)
+        {
+            result += $"\"{header}\";";
+        }
+        foreach (GeraldicSign elem in data)
+        {
+            result += elem.ToString();
+        }
+
+        return result;
+    }
+}
