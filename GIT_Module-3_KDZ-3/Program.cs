8303e69ef71e729adb11d4039938c758c3cee1b2
Teplyakov Vladislav Vitalievich <somi.saaw@mail.ru>
2024-03-15 23:56:52 +0300

    Добавил запуск телеграмм бота из инструкции

--- GIT_Module-3_KDZ-3/Program.cs
+++ GIT_Module-3_KDZ-3/Program.cs
@@ -1,10 +1,13 @@
 ﻿using Telegram.Bot;
 using Telegram.Bot.Types;
+using Telegram.Bot.Types.ReplyMarkups;
 internal class Program
 {
+    private static string token { get; set; } = "6757896301:AAGpTPCVQBwt0dkJN5wYG4MD3dCmmUJWGyg";
+    private static TelegramBotClient client;
     static void Main(string[] args)
     {
-        var client = new TelegramBotClient("6757896301:AAGpTPCVQBwt0dkJN5wYG4MD3dCmmUJWGyg");
+        client = new TelegramBotClient(token);
         client.StartReceiving(Update, Error);
         Console.ReadLine();
     }
@@ -12,14 +15,24 @@ internal class Program
     async static Task Update(ITelegramBotClient botClient, Update update, CancellationToken token)
     {
         var message = update.Message;
-        if (message.Text == null)
+        if (message.Text != null)
         {
-            if (message.Text.ToLower().Contains("здорова"))
-            {
-                await botClient.SendTextMessageAsync(message.Chat.Id, "Здорова пользователь");
-                return;
-            }
+            await botClient.SendTextMessageAsync(message.Chat.Id, "Здорова пользователь", replyMarkup: GetButtons());
+            return;
         }
+    }
+
+    private static IReplyMarkup? GetButtons()
+    {
+        return new ReplyKeyboardMarkup
+        (   
+            new List<List<KeyboardButton>>
+            {
+                new List<KeyboardButton>{ new KeyboardButton("Загрузить CSV файл"), new KeyboardButton ("Выборка") },
+                new List<KeyboardButton>{ new KeyboardButton("Сортировка"), new KeyboardButton ("Скачать обработанный файл")},
+                new List<KeyboardButton>{ new KeyboardButton("Загрузить JSON файл")}
+            }
+        );
     }
 
     async static Task Error(ITelegramBotClient client, Exception exception, CancellationToken token)
