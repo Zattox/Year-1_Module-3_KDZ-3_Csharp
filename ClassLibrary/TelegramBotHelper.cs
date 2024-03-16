8cdb91fc3968bdb259abe26411e0baf5350f33a9
Teplyakov Vladislav Vitalievich <somi.saaw@mail.ru>
2024-03-16 16:32:31 +0300

    Изменил меню взаимодействия

--- ClassLibrary/TelegramBotHelper.cs
+++ ClassLibrary/TelegramBotHelper.cs
@@ -1,16 +1,118 @@
 ﻿using Telegram.Bot;
-using Telegram.Bot.Polling;
 using Telegram.Bot.Types;
 using Telegram.Bot.Types.ReplyMarkups;
 public class TelegramBotHelper
 {
     private string token;
-    static GeraldicSignList table = new GeraldicSignList();
+    static GeraldicSignList table;
     TelegramBotClient client;
     public TelegramBotHelper(string token)
     {
         this.token = token;
     }
+
+    private static IReplyMarkup? GetInputButtons()
+    {
+        return new ReplyKeyboardMarkup(
+            new List<List<KeyboardButton>>
+            {
+                new List<KeyboardButton> { new KeyboardButton("Загрузить CSV файл"), new KeyboardButton("Загрузить JSON файл") }
+            }
+        );
+    }
+    private static IReplyMarkup? GetButtons()
+    {
+        return new ReplyKeyboardMarkup(
+            new List<List<KeyboardButton>>
+            {
+                new List<KeyboardButton> { new KeyboardButton("Загрузить CSV файл"), new KeyboardButton("Загрузить JSON файл")  },
+                new List<KeyboardButton> { new KeyboardButton("Фильтрация по Type"), new KeyboardButton("Фильтрация по RegistrationDate") },
+                new List<KeyboardButton> { new KeyboardButton("Фильтрация по CertificateHolderName и RegistrationDate") },
+                new List<KeyboardButton> { new KeyboardButton("Сортировка по возрастанию"), new KeyboardButton ("Сортировка по убыванию") },
+                new List<KeyboardButton> { new KeyboardButton("Скачать обработанный файл в JSON"), new KeyboardButton("Скачать обработанный файл в СSV") }
+            }
+        );
+    }
+    private async void ProcessUpdate(ITelegramBotClient botClient, Update update)
+    {
+        switch (update.Type)
+        {
+            case Telegram.Bot.Types.Enums.UpdateType.Message:
+                var text = update.Message.Text;
+                if (table is null)
+                {
+                    switch (text)
+                    {
+                        case "Загрузить CSV файл":
+                            await client.SendTextMessageAsync(update.Message.Chat.Id, "Загрузить CSV файл", replyMarkup: GetInputButtons());
+                            break;
+
+                        case "Загрузить JSON файл":
+                            await client.SendTextMessageAsync(update.Message.Chat.Id, "Загрузить JSON файл", replyMarkup: GetInputButtons());
+                            break;
+
+                        default:
+                            await client.SendTextMessageAsync(update.Message.Chat.Id, "Для других функции для начала загрузите данные из файла!", replyMarkup: GetInputButtons());
+                            break;
+                    }
+                }
+                else
+                {
+                    switch (text)
+                    {
+                        case "Загрузить CSV файл":
+                            await client.SendTextMessageAsync(update.Message.Chat.Id, "Загрузить CSV файл", replyMarkup: GetButtons());
+                            break;
+
+                        case "Загрузить JSON файл":
+                            await client.SendTextMessageAsync(update.Message.Chat.Id, "Загрузить JSON файл", replyMarkup: GetButtons());
+                            break;
+
+                        case "Фильтрация по Type":
+                            await client.SendTextMessageAsync(update.Message.Chat.Id, "Фильтрация по Type", replyMarkup: GetButtons());
+                            await FilteringData.FilterByOneCondition(botClient, update, "Type", table);
+                            break;
+
+                        case "Фильтрация по RegistrationDate":
+                            await client.SendTextMessageAsync(update.Message.Chat.Id, "Фильтрация по RegistrationDate", replyMarkup: GetButtons());
+                            await FilteringData.FilterByOneCondition(botClient, update, "RegistrationDate", table);
+                            break;
+
+                        case "Фильтрация по CertificateHolderName и RegistrationDate":
+                            await client.SendTextMessageAsync(update.Message.Chat.Id, "Фильтрация по CertificateHolderName и RegistrationDate", replyMarkup: GetButtons());
+                            await FilteringData.FilterByTwoConditions(botClient, update, "CertificateHolderName", "RegistrationDate", table);
+                            break;
+
+                        case "Сортировка по возрастанию":
+                            await client.SendTextMessageAsync(update.Message.Chat.Id, "Сортировка по возрастанию", replyMarkup: GetButtons());
+                            break;
+
+                        case "Сортировка по убыванию":
+                            await client.SendTextMessageAsync(update.Message.Chat.Id, "Сортировка по убыванию", replyMarkup: GetButtons());
+                            break;
+
+                        case "Скачать обработанный файл в JSON":
+                            await client.SendTextMessageAsync(update.Message.Chat.Id, "Скачать обработанный файл в JSON", replyMarkup: GetButtons());
+                            break;
+
+                        case "Скачать обработанный файл в CSV":
+                            await client.SendTextMessageAsync(update.Message.Chat.Id, "Скачать обработанный файл в CSV", replyMarkup: GetButtons());
+                            break;
+
+                        default:
+                            await client.SendTextMessageAsync(update.Message.Chat.Id, "Такой функции нет", replyMarkup: GetButtons());
+                            break;
+                    }
+                }
+                break;
+
+            default:
+                Console.WriteLine(update.Type + " Not implemented!");
+                break;
+
+        }
+    }
+
     public void GetUpdates()
     {
         client = new TelegramBotClient(token);
@@ -25,9 +127,9 @@ public class TelegramBotHelper
                     var updates = client.GetUpdatesAsync(offset).Result;
                     if (updates != null && updates.Count() > 0)
                     {
-                        foreach(var update in updates)
+                        foreach (var update in updates)
                         {
-                            processUpdate(client, update);
+                            ProcessUpdate(client, update);
                             offset = update.Id + 1;
                         }
                     }
@@ -39,81 +141,5 @@ public class TelegramBotHelper
                 Thread.Sleep(1000);
             }
         }
-    }
-
-    private async void processUpdate(ITelegramBotClient botClient, Update update)
-    {
-        switch (update.Type)
-        {
-            case Telegram.Bot.Types.Enums.UpdateType.Message:
-                var text = update.Message.Text;
-                switch (text)
-                {
-                    case "Загрузить CSV файл":
-                        await client.SendTextMessageAsync(update.Message.Chat.Id, "Загрузить CSV файл", replyMarkup: GetButtons());
-                        break;
-
-                    case "Выборка":
-                        await client.SendTextMessageAsync(update.Message.Chat.Id, "Выборка", replyMarkup: GetChooseFilterButtons());
-                        break;
-
-                    case "Сортировка":
-                        await client.SendTextMessageAsync(update.Message.Chat.Id, "Сортировка", replyMarkup: GetButtons());
-                        break;
-
-                    case "Скачать обработанный файл":
-                        await client.SendTextMessageAsync(update.Message.Chat.Id, "Скачать обработанный файл", replyMarkup: GetButtons());
-                        break;
-
-                    case "Загрузить JSON файл":
-                        await client.SendTextMessageAsync(update.Message.Chat.Id, "Загрузить JSON файл", replyMarkup: GetButtons());
-                        break;
-
-                    case "Фильтрация по Type":
-                        await FilteringData.FilterByOneCondition(botClient, update, "Type", table);
-                        break;  
-
-                    case "Фильтрация по RegistrationDate":
-                        await FilteringData.FilterByOneCondition(botClient, update, "RegistrationDate", table);
-                        break;
-
-                    case "Фильтрация по CertificateHolderName и RegistrationDate":
-                        await FilteringData.FilterByTwoConditions(botClient, update, "CertificateHolderName", "RegistrationDate", table);
-                        break;
-
-                    default:
-                        await client.SendTextMessageAsync(update.Message.Chat.Id, "Такой кнопки нет", replyMarkup: GetButtons());
-                        break;
-                }
-                break;
-
-            default:
-                Console.WriteLine(update.Type + " Not implemented!");
-                break;
-
-        }
-    }
-    private static IReplyMarkup? GetChooseFilterButtons()
-    {
-        return new ReplyKeyboardMarkup
-        (
-            new List<List<KeyboardButton>>
-            {
-                new List<KeyboardButton> { new KeyboardButton ("Фильтрация по Type") },
-                new List<KeyboardButton> { new KeyboardButton ("Фильтрация по RegistrationDate") },
-                new List<KeyboardButton> { new KeyboardButton ("Фильтрация по CertificateHolderName и RegistrationDate") }
-            }
-        );
-    }
-    private static IReplyMarkup? GetButtons()
-    {
-        return new ReplyKeyboardMarkup(
-            new List<List<KeyboardButton>>
-            { 
-                new List<KeyboardButton> { new KeyboardButton("Загрузить CSV файл"), new KeyboardButton ("Выборка") },
-                new List<KeyboardButton> { new KeyboardButton("Сортировка"), new KeyboardButton ("Скачать обработанный файл") },
-                new List<KeyboardButton> { new KeyboardButton("Загрузить JSON файл") }
-            }
-        );
     }
 }
