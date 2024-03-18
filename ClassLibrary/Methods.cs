cadcaf8efdbb1226768745928e79f767e1028c36
Teplyakov Vladislav Vitalievich <somi.saaw@mail.ru>
2024-03-18 11:00:00 +0300

    Добавил метод для нахождения пути до папки с файлами, хранящие данные

--- ClassLibrary/Methods.cs
+++ ClassLibrary/Methods.cs
@@ -1,5 +1,23 @@
-﻿internal class Methods
+﻿using System.Diagnostics;
+internal class Methods
 {
+    internal static string FindExecutablePath()
+    {
+        Process currentProcess = Process.GetCurrentProcess();
+        string executablePath = currentProcess.MainModule.FileName;
 
+        executablePath = Path.GetDirectoryName(executablePath);
+        executablePath = Path.GetDirectoryName(executablePath);
+        executablePath = Path.GetDirectoryName(executablePath);
+        executablePath = Path.GetDirectoryName(executablePath);
+        executablePath = Path.GetDirectoryName(executablePath);
+
+        executablePath += "\\data";
+        if (!Directory.Exists(executablePath))
+        {
+            Directory.CreateDirectory(executablePath);
+        }
+
+        return executablePath;
+    }
 }
-
