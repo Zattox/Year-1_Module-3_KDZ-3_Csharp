e882a7866b094c5f8cbf78f9caeb8d93126e37ba
Teplyakov Vladislav Vitalievich <somi.saaw@mail.ru>
2024-03-18 12:03:21 +0300

    Добавил класс с константами

---
+++ ClassLibrary/AppConstants.cs
@@ -0,0 +1,21 @@
+﻿internal class AppConstants
+{
+    public const int CountOfHeaders = 9;
+
+    public const string FilterButtonText1 = "Фильтрация по Type";
+    public const string FilterButtonText2 = "Фильтрация по RegistrationDate";
+    public const string FilterButtonText3 = "Фильтрация по CertificateHolderName и RegistrationDate";
+
+    public const string SortingButtonText1 = "Сортировка по RegistrationNumber в порядке возрастания номера";
+    public const string SortingButtonText2 = "Сортировка по RegistrationNumber в порядке убывания номера";
+
+    public const string OutputButtonText1 = "Скачать последний обработанный файл в CSV";
+    public const string OutputButtonText2 = "Скачать последний обработанный файл в JSON";
+
+    public const string MenuButtonText1 = "Выборка";
+    public const string MenuButtonText2 = "Сортировка";
+    public const string MenuButtonText3 = "Скачать последний обработанный файл";
+    public const string MenuButtonText4 = "Загрузить новый файл";
+
+}
+
