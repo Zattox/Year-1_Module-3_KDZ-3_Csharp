e047f0cf27414cb40db93db23b2a8564a470d797
Teplyakov Vladislav Vitalievich <somi.saaw@mail.ru>
2024-03-18 12:03:45 +0300

    Выделил методы для вывода кнопок в чате телеграмма, как отдельный класс

---
+++ ClassLibrary/Buttons.cs
@@ -0,0 +1,49 @@
+﻿using Telegram.Bot.Types.ReplyMarkups;
+using static AppConstants;
+internal class Buttons
+{
+    internal static IReplyMarkup? GetFilterButtons()
+    {
+        return new ReplyKeyboardMarkup(
+            new List<List<KeyboardButton>>
+            {
+                new List<KeyboardButton> { new KeyboardButton(FilterButtonText1) },
+                new List<KeyboardButton> { new KeyboardButton(FilterButtonText2) },
+                new List<KeyboardButton> { new KeyboardButton(FilterButtonText3) }
+            }
+        );
+    }
+    internal static IReplyMarkup? GetSortingButtons()
+    {
+        return new ReplyKeyboardMarkup(
+            new List<List<KeyboardButton>>
+            {
+                new List<KeyboardButton> { new KeyboardButton(SortingButtonText1) },
+                new List<KeyboardButton> { new KeyboardButton(SortingButtonText2) }
+            }
+        );
+    }
+    internal static IReplyMarkup? GetOutputButtons()
+    {
+        return new ReplyKeyboardMarkup(
+            new List<List<KeyboardButton>>
+            {
+                new List<KeyboardButton> { new KeyboardButton(OutputButtonText1) },
+                new List<KeyboardButton> { new KeyboardButton(OutputButtonText2) }
+            }
+        );
+    }
+    internal static IReplyMarkup? GetMenuButtons()
+    {
+        return new ReplyKeyboardMarkup(
+            new List<List<KeyboardButton>>
+            {
+                new List<KeyboardButton> { new KeyboardButton(MenuButtonText1) },
+                new List<KeyboardButton> { new KeyboardButton(MenuButtonText2) },
+                new List<KeyboardButton> { new KeyboardButton(MenuButtonText3) },
+                new List<KeyboardButton> { new KeyboardButton(MenuButtonText4) }
+            }
+        );
+    }
+}
+
