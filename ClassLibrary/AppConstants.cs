0dc51934f24e67dd256311b484e9056e8d9e508f
Teplyakov Vladislav Vitalievich <somi.saaw@mail.ru>
2024-03-19 22:32:58 +0300

    Теперь ВСЕ поля нельзя ИЗМЕНЯТЬ

--- ClassLibrary/AppConstants.cs
+++ ClassLibrary/AppConstants.cs
@@ -1,19 +1,14 @@
-﻿public class AppConstants
-{
+﻿using System.Diagnostics;
+
+public class AppConstants
+{   
     // Общие константы решения.
     public const int CountOfHeaders = 9; // Количество столбцов/заголовков в csv файле.
     public const string Token = "6757896301:AAGpTPCVQBwt0dkJN5wYG4MD3dCmmUJWGyg"; // Токен для связи с ботом.
     public const string Separator = "\";\""; // Разделитель в csv файле.
 
-    // Константы для абсолютных путей до нужных директорий. (Иницилизируется при помощи метода Methods.CreateDirectories)
-    public static string ExecutablePath = string.Empty; // Путь до папки проекта.
-    public static string OutputJSONPath = ""; // Путь до папки, где хранятся файлы для вывода JSON.
-    public static string OutputCSVPath = ""; // Путь до папки, где хранятся файлы для вывода CSV.
-    public static string DataPath = ""; // Путь до папки, где хранятся все файлы с данными.
-    public static string LogPath = ""; // Путь до папки, где хранятся файлы для логирования.
-
     // Тексты для кнопок основного меню.
-    public const string MenuButtonText1 = "Загрузить новый файл";
+    public const string MenuButtonText1 = "Загрузить новый файл"; 
     public const string MenuButtonText2 = "Выборка";
     public const string MenuButtonText3 = "Сортировка";
     public const string MenuButtonText4 = "Скачать последний обработанный файл";
@@ -44,4 +39,59 @@
                                           $"{FilterButtonText1} \"значение поля\"\n\n" +
                                           $"{FilterButtonText2} \"значение поля\"\n\n" +
                                           $"{FilterButtonText3} \"значение поля\" \"значение поля\" ";
+
+    // Константы для абсолютных путей до нужных директорий. (Иницилизируется при помощи метода Methods.CreateDirectories)
+    public static readonly string ExecutablePath; // Путь до папки проекта.
+    public static readonly string OutputJSONPath; // Путь до папки, где хранятся файлы для вывода JSON.
+    public static readonly string OutputCSVPath;  // Путь до папки, где хранятся файлы для вывода CSV.
+    public static readonly string DataPath;       // Путь до папки, где хранятся все файлы с данными.
+    public static readonly string LogPath;        // Путь до папки, где хранятся файлы для логирования.
+
+    /// <summary>
+    /// Создания нужных директорий для работы программы. Поиск абсолютных путей до них.
+    /// </summary>
+
+    static AppConstants()
+    {
+        // Cимвол, задаваемый платформой, для разделения уровней папок в строке пути.
+        char separatorSym = Path.DirectorySeparatorChar;
+
+        // Поиск абсолютного пути до запускаемого файла проекта.
+        Process currentProcess = Process.GetCurrentProcess();
+        ExecutablePath = currentProcess.MainModule.FileName;
+
+        // Поднятие вверх по директориям на уровень папки проекта.
+        for (int i = 0; i < 5; ++i)
+        {
+            ExecutablePath = Path.GetDirectoryName(ExecutablePath);
+        }
+
+        // Создание директории \data. (Хранение всех файлов с данными)
+        DataPath = ExecutablePath + $"{separatorSym}data";
+        if (!Directory.Exists(DataPath))
+        {
+            Directory.CreateDirectory(DataPath);
+        }
+
+        // Создание директории \data\JSONOutput. (Хранение файлов для вывода данных с .json)
+        OutputJSONPath = DataPath + $"{separatorSym}JSONOutput";
+        if (!Directory.Exists(OutputJSONPath))
+        {
+            Directory.CreateDirectory(OutputJSONPath);
+        }
+
+        // Создание директории \data\CSVOutput.(Хранение файлов для вывода данных с .csv)
+        OutputCSVPath = DataPath + $"{separatorSym}CSVOutput";
+        if (!Directory.Exists(OutputCSVPath))
+        {
+            Directory.CreateDirectory(OutputCSVPath);
+        }
+
+        // Создание директории \var.(Хранение файлов для вывода логирования)
+        LogPath = ExecutablePath + $"{separatorSym}var";
+        if (!Directory.Exists(LogPath))
+        {
+            Directory.CreateDirectory(LogPath);
+        }
+    }
 }
