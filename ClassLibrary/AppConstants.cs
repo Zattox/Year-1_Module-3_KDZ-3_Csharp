314c89ba77d5d610617f40333bec8ba53ee84bd9
Teplyakov Vladislav Vitalievich <somi.saaw@mail.ru>
2024-03-19 15:03:35 +0300

    Добавил комментарии к константам

--- ClassLibrary/AppConstants.cs
+++ ClassLibrary/AppConstants.cs
@@ -2,31 +2,38 @@
 
 public class AppConstants
 {
-    public const int CountOfHeaders = 9;
-    public const string Token = "6757896301:AAGpTPCVQBwt0dkJN5wYG4MD3dCmmUJWGyg";
-    public const string Separator = "\";\"";
+    // Общие константы решения.
+    public const int CountOfHeaders = 9; // Количество столбцов/заголовков в csv файле.
+    public const string Token = "6757896301:AAGpTPCVQBwt0dkJN5wYG4MD3dCmmUJWGyg"; // Токен для связи с ботом.
+    public const string Separator = "\";\""; // Разделитель в csv файле.
 
-    public static string ExecutablePath = string.Empty;
-    public static string OutputJSONPath = "";
-    public static string OutputCSVPath = "";
-    public static string DataPath = "";
-    public static string LogPath = "";
+    // Константы для абсолютных путей до нужных директорий. (Иницилизируется при помощи метода Methods.CreateDirectories)
+    public static string ExecutablePath = string.Empty; // Путь до папки проекта.
+    public static string OutputJSONPath = ""; // Путь до папки, где хранятся файлы для вывода JSON.
+    public static string OutputCSVPath = ""; // Путь до папки, где хранятся файлы для вывода CSV.
+    public static string DataPath = ""; // Путь до папки, где хранятся все файлы с данными.
+    public static string LogPath = ""; // Путь до папки, где хранятся файлы для логирования.
 
-    public const string MenuButtonText1 = "Загрузить новый файл";
+    // Тексты для кнопок основного меню.
+    public const string MenuButtonText1 = "Загрузить новый файл"; 
     public const string MenuButtonText2 = "Выборка";
     public const string MenuButtonText3 = "Сортировка";
     public const string MenuButtonText4 = "Скачать последний обработанный файл";
 
+    // Тектсы для кнопок *меню* фильтрации.
     public const string FilterButtonText1 = "Фильтрация по Type";
     public const string FilterButtonText2 = "Фильтрация по RegistrationDate";
     public const string FilterButtonText3 = "Фильтрация по CertificateHolderName и RegistrationDate";
 
+    // Тексты для кнопок меню фильтрации.
     public const string SortingButtonText1 = "Сортировка по RegistrationNumber в порядке возрастания номера";
     public const string SortingButtonText2 = "Сортировка по RegistrationNumber в порядке убывания номера";
 
+    // Тексты для кнопок меню загрузки файла.
     public const string OutputButtonText1 = "Скачать последний обработанный файл в JSON";
     public const string OutputButtonText2 = "Скачать последний обработанный файл в CSV";
 
+    // Вспомогательные сообщения для пользователя.
     public const string InputMessage = "Загрузите файл в формате JSON или CSV ";
     public const string ErrorMessage = "Произошла ошибка во время исполнения программы! ";
     public const string StoppedMessage = "Для других функции для начала загрузите файла c данными! ";
