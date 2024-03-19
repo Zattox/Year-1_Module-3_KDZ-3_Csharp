e23aed0eca2761b8157c8b9b72ed9ce925f4bcdb
Teplyakov Vladislav Vitalievich <somi.saaw@mail.ru>
2024-03-19 22:51:11 +0300

    Перенес метод CreateDirectories в статический конструткор класса AppConstants

--- ClassLibrary/Methods.cs
+++ ClassLibrary/Methods.cs
@@ -11,8 +11,6 @@ public class Methods
     /// </summary>
     static Methods()
     {
-        CreateDirectories();
-
         logFilePath += LogPath + $"\\Console_log_{DateTime.Now:dd-MM}.txt";
         if (!File.Exists(logFilePath))
         {
@@ -28,53 +26,6 @@ public class Methods
             });
 
             logger = loggerFactory.CreateLogger<Methods>();
-        }
-    }
-
-    /// <summary>
-    /// Создания нужных директорий для работы программы. Поиск абсолютных путей до них.
-    /// </summary>
-    public static void CreateDirectories()
-    {
-        // Cимвол, задаваемый платформой, для разделения уровней папок в строке пути.
-        char separatorSym = Path.DirectorySeparatorChar;
-
-        // Поиск абсолютного пути до запускаемого файла проекта.
-        Process currentProcess = Process.GetCurrentProcess();
-        ExecutablePath = currentProcess.MainModule.FileName;
-
-        // Поднятие вверх по директориям на уровень папки проекта.
-        for (int i = 0; i < 5; ++i)
-        {
-            ExecutablePath = Path.GetDirectoryName(ExecutablePath);
-        }
-
-        // Создание директории \data. (Хранение всех файлов с данными)
-        DataPath = ExecutablePath + $"{separatorSym}data";
-        if (!Directory.Exists(DataPath))
-        {
-            Directory.CreateDirectory(DataPath);
-        }
-
-        // Создание директории \data\JSONOutput. (Хранение файлов для вывода данных с .json)
-        OutputJSONPath = DataPath + $"{separatorSym}JSONOutput";
-        if (!Directory.Exists(OutputJSONPath))
-        {
-            Directory.CreateDirectory(OutputJSONPath);
-        }
-
-        // Создание директории \data\CSVOutput.(Хранение файлов для вывода данных с .csv)
-        OutputCSVPath = DataPath + $"{separatorSym}CSVOutput";
-        if (!Directory.Exists(OutputCSVPath))
-        {
-            Directory.CreateDirectory(OutputCSVPath);
-        }
-
-        // Создание директории \var.(Хранение файлов для вывода логирования)
-        LogPath = ExecutablePath + $"{separatorSym}var";
-        if (!Directory.Exists(LogPath))
-        {
-            Directory.CreateDirectory(LogPath);
         }
     }
 
